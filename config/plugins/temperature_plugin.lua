ba329156f4d81ddd72bde183833d70d2e55d00aa
Gleb Shepelev <shepelev@restream.rt.ru>
2018-04-09 16:55:43 +0300

    TASK: Температурный плагин

---
+++ config/plugins/temperature_plugin.lua
@@ -0,0 +1,17 @@
+function get_max_temp()
+  local data = {}
+  for _, file in ipairs(filepath.glob("/sys/bus/platform/devices/coretemp*/temp*_input")) do
+    local temp = tonumber(ioutil.readfile(file))/1000
+    table.insert(data, temp)
+  end
+  table.sort(data)
+  return data[#data]
+end
+
+-- главный loop
+while true do
+  local maxt = get_max_temp()
+  metrics.set("system.cpu.temp", maxt)
+
+  time.sleep(10)
+end
