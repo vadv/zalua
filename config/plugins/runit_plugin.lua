0b80adbf52d3bacd461c0095a21ac2e7fe5619b4
Dmitry Vasilyev <vadv.mkn@gmail.com>
2018-04-17 17:17:59 +0300

    runit plugin

--- config/plugins/runit_plugin.lua
+++ config/plugins/runit_plugin.lua
@@ -21,20 +21,20 @@ while true do
           -- подозрительный сервис
           if services[name] and (services[name] < 60) then
             -- был до этого уже замечен, отмечаем как флапающий
-            local desc = "'"..name.."' has flapping uptime"
+            local desc = "'"..name.."' перезапускается"
             if problem == '' then problem = desc else problem = problem..', '..desc end
           end
         end
       else
         -- процесс слинкован, но не запущен и это уже плохо
-        local desc = "'"..name.."' has linked, but isn't running"
+        local desc = "'"..name.."' был слинкован в /etc/services/, но не запущен"
         if problem == '' then problem = desc else problem = problem..', '..desc end
       end
       services[name] = uptime
 
     end
 
-    if problem == '' then problem = 'ok' else problem = 'Found problem with runit services: '..problem end
+    if problem == '' then problem = 'ok' else problem = 'Найдены проблемы с runit сервисами: '..problem end
     metrics.set('runit.problem', problem)
   else
     metrics.set('runit.problem', 'ok')
